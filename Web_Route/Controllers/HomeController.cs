03f4967ff3bd4d060afe09744819ef162592e3f5
Okan Candeniz <candenizokan@gmail.com>
2023-06-18 13:36:15 +0300

    detay.cshtml, home deneme actionresult

--- Web_Route/Controllers/HomeController.cs
+++ Web_Route/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace Web_Route.Controllers
             return View();
         }
 
+        public IActionResult Deneme(string isim, string id)
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
