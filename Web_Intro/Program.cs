8e8cd120d28cb9542c1cfeec6f682608b6305661
Okan Candeniz <candenizokan@gmail.com>
2023-06-18 12:14:58 +0300

    program.cs

--- Web_Intro/Program.cs
+++ Web_Intro/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -11,10 +11,19 @@ namespace Web_Intro
 {
     public class Program
     {
+        /*
+            Programımızı default olarak ayağa kaldıran çalışan ilk metod MAIN metoddur.
+        .NETCORE ile birlikte web uygulamaları artık her platformda çalışır hale geldi.
+        Bu nedenle hangi ortamda çalışmak istersek buradaki CreateHostBuilder() metodunu düzenleyerek farklı server uygulamamızı ayağ kaldırabiliriz. Default olarak ISS (Internet Information Server) üzerinden çalışır.
+
+         */
         public static void Main(string[] args)
         {
+            //MAIN içindeki bu metod sayesinde string argumanları alarak komutları çalıştırır.
             CreateHostBuilder(args).Build().Run();
         }
+
+        //CreateHostBuilder() metoduna bakıldığında ise yapacağı işlemleri, okuyacağı konfigurasyonlar için starup sınıfına gider ve orada kullanılan Configure ve ConfigureServices metodlarındaki ayarları okuyarak uygulamayı ayağa kaldırı. build edilir ce çalıştırılır MAIN içinde
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
