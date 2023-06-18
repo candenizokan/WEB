c22c68e7310d07fb4ade04a01ec1f14a833dd5ac
Okan Candeniz <candenizokan@gmail.com>
2023-06-18 12:34:59 +0300

    add kategori controller

---
+++ Web_Route/Controllers/KategoriController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Route.Controllers
+{
+    public class KategoriController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
