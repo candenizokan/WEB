64cad63b625eb8b9a8aad99b905238a0ec046a50
Okan Candeniz <candenizokan@gmail.com>
2023-06-18 13:05:47 +0300

    add IActionResult Test

--- Web_Route/Controllers/HomeController.cs
+++ Web_Route/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace Web_Route.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
