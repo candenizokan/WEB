a20d40994cf0add37503086d070e180430a42266
Okan Candeniz <candenizokan@gmail.com>
2023-06-18 13:03:05 +0300

    add kontrol view

--- Web_Route/Controllers/KategoriController.cs
+++ Web_Route/Controllers/KategoriController.cs
@@ -4,6 +4,11 @@ namespace Web_Route.Controllers
 {
     public class KategoriController : Controller
     {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
         public IActionResult Kontrol()
         {
             return View();
