2da0d3f2f1f39da7dfb2edd90d836171e20544ea
Okan Candeniz <candenizokan@gmail.com>
2023-07-08 13:35:45 +0300

    product reposityor

--- IOC/Infrastructure/Repositories/Concrete/ProductRepository.cs
+++ IOC/Infrastructure/Repositories/Concrete/ProductRepository.cs
@@ -1,27 +1,37 @@
 ﻿using IOC.Infrastructure.Repositories.Abstract;
 using IOC.Models;
 using IOC.Models.Context;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace IOC.Infrastructure.Repositories.Concrete
 {
     public class ProductRepository : IRepository<Product>
     {
         //ihtiyacı newleyerek yapmamalısın ProjectContext new 
-        
+
+        private readonly ProjectContext _context;
+
+        public ProductRepository(ProjectContext context)
+        {
+            _context = context;// dışarıdan gelen contexti buradaki sınıfta kullan
+        }
         public void Add(Product entity)
         {
             //projetcontest sınıfı üzerinden ekle demem lazım
+
+            _context.Products.Add(entity);
         }
 
         public IList<Product> GetAll()
         {
-            throw new System.NotImplementedException();
+            return _context.Products.Include(a => a.Category).Include(a => a.Supplier).ToList();
         }
 
         public int Save()
         {
-            throw new System.NotImplementedException();
+            return _context.SaveChanges();
         }
     }
 }
