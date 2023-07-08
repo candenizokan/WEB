dfa9f91844cf79ef4b456c640487e1239a3c2457
Okan Candeniz <candenizokan@gmail.com>
2023-07-08 11:23:27 +0300

    supplier

--- IOC/Models/Supplier.cs
+++ IOC/Models/Supplier.cs
@@ -1,6 +1,13 @@
-﻿namespace IOC.Models
+﻿using System.Collections.Generic;
+
+namespace IOC.Models
 {
     public class Supplier
     {
+        public int SupplierId { get; set; }
+        public string Name { get; set; }
+
+        //navigation property
+        public ICollection<Product> Products { get; set; }
     }
 }
