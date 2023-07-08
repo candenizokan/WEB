43282d4b5e5f0e8ee5c0d5c36a742e76311a0b34
Okan Candeniz <candenizokan@gmail.com>
2023-07-08 11:21:37 +0300

    category

--- IOC/Models/Category.cs
+++ IOC/Models/Category.cs
@@ -1,6 +1,14 @@
-﻿namespace IOC.Models
+﻿using System.Collections.Generic;
+
+namespace IOC.Models
 {
     public class Category
     {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+
+        //bir kategorinin çokça ürünü olur. çokluk yapısını koleksiyon yapısında tutarım
+
+        public ICollection<Product> Products { get; set; }
     }
 }
