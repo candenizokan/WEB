b5fdfff6dda502a6b2cab6377a0f4c9288be956b
Okan Candeniz <candenizokan@gmail.com>
2023-07-08 13:14:25 +0300

    ireposityorty

--- IOC/Infrastructure/Repositories/Abstract/IRepository.cs
+++ IOC/Infrastructure/Repositories/Abstract/IRepository.cs
@@ -1,7 +1,14 @@
-﻿namespace IOC.Infrastructure.Repositories.Abstract
-{
-    public interface IRepository
-    {
+﻿using System.Collections.Generic;
 
+namespace IOC.Infrastructure.Repositories.Abstract
+{
+    //generic type kullanmış oldum. T sınıfı şu an bu interface içinde belli değil ama implemente edileceği yerde hangi sınıf seçilirse o sınıf için aşağıdaki metodlar şekillendirilebilir olmuş olacak
+    //generic type sınıf, interface, koleksiyon yapıları vb kullanabilir
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        int Save();
+
+        IList<T> GetAll();
     }
 }
