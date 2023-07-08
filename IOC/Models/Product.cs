0782e025caa150f9d1ed365eb34933c2b0e95789
Okan Candeniz <candenizokan@gmail.com>
2023-07-08 11:06:40 +0300

    product  sınıfı notlar

--- IOC/Models/Product.cs
+++ IOC/Models/Product.cs
@@ -6,10 +6,15 @@
         public string Name { get; set; }
         public int Stock { get; set; }
 
-        public int CategoryId { get; set; }
+        // navigation prop => bu sınıfın başka sınıflarla olan ilişkisini veritabanında açıklamk için kullanılan proplar.
+        public int? CategoryId { get; set; } // ? anlamı şudur buradaki property nullable oluşur
         public Category Category { get; set; }
 
         public int SupplierId { get; set; }
         public Supplier Supplier { get; set; }
+
+        // iki şekilde veri çekebilirim. LAZY LOADING ve EAGER LOAFING
+        // defaultta yukarıdaki gibi yaparsam EAGER yapar.
+        // LAZY olmasını isterse VIRTUAL keyword eklenmeli ve proxy paketi eklenmelidir
     }
 }
