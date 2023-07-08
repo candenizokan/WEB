b0cb1e0039ab2b8a397d3ad0846372a58f2d7f22
Okan Candeniz <candenizokan@gmail.com>
2023-07-08 14:10:55 +0300

    CreateProductVM

---
+++ IOC/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IOC.Controllers
+{
+    public class ProductController : Controller
+    {
+       [HttpGet]//defaultta httpget olarak kabul eder
+       public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ IOC/Models/VMs/CreateProductVM.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace IOC.Models.VMs
+{
+    public class CreateProductVM
+    {
+        //1 tane product nesne
+
+        public Product Product { get; set; }
+
+        //tüm kategoriler
+
+        public List<SelectListItem> Categories { get; set; }
+
+        //tüm tedarikçileri
+        public List<SelectListItem> Suppliers { get; set; }
+    }
+}
