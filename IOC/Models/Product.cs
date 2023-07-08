5f5167cd32e2481ccefb7092d99e04b7f9207385
Okan Candeniz <candenizokan@gmail.com>
2023-07-08 11:01:35 +0300

    product sınıfım

--- IOC/Models/Product.cs
+++ IOC/Models/Product.cs
@@ -2,5 +2,11 @@
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
