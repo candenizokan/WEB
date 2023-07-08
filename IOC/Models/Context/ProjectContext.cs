556ccafc5d69580a75d277c83c12b86475658150
Okan Candeniz <candenizokan@gmail.com>
2023-07-08 12:01:21 +0300

    DbContext

---
+++ IOC/Models/Context/ProjectContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IOC.Models.Context
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> options):base(options)
+        {
+
+        }
+    }
+}
