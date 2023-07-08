3f050d6469fb39b48ac30d26479d00b8cc584c66
Okan Candeniz <candenizokan@gmail.com>
2023-07-08 13:37:01 +0300

    add category repository

---
+++ IOC/Infrastructure/Repositories/Concrete/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using IOC.Infrastructure.Repositories.Abstract;
+using IOC.Models;
+using System.Collections.Generic;
+
+namespace IOC.Infrastructure.Repositories.Concrete
+{
+    public class CategoryRepository : IRepository<Category>
+    {
+
+
+        public void Add(Category entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Category> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Save()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
