23fd5459c088ebdcef77d765e7a7332130809d8d
Okan Candeniz <candenizokan@gmail.com>
2023-07-08 13:50:02 +0300

    SupplierRepository

---
+++ IOC/Infrastructure/Repositories/Concrete/SupplierRepository.cs
@@ -0,0 +1,32 @@
+﻿using IOC.Infrastructure.Repositories.Abstract;
+using IOC.Models;
+using IOC.Models.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IOC.Infrastructure.Repositories.Concrete
+{
+    public class SupplierRepository : IRepository<Supplier>
+    {
+        private readonly ProjectContext _context;
+        public SupplierRepository(ProjectContext context)
+        {
+            _context = context;
+        }
+        public void Add(Supplier entity)
+        {
+            _context.Suppliers.Add(entity);
+        }
+
+        public IList<Supplier> GetAll()
+        {
+            return _context.Suppliers.Include(a => a.Products).ToList();
+        }
+
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
+    }
+}
