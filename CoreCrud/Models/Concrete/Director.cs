df4c8a9584b3bb94f7f7216b5cfbca99cd92c4ea
Okan Candeniz <candenizokan@gmail.com>
2023-07-09 13:39:50 +0300

    Director model

--- CoreCrud/Models/Concrete/Director.cs
+++ CoreCrud/Models/Concrete/Director.cs
@@ -1,6 +1,27 @@
-﻿namespace CoreCrud.Models.Concrete
+﻿using CoreCrud.Models.Abstract;
+using System;
+using System.Collections.Generic;
+
+namespace CoreCrud.Models.Concrete
 {
-    public class Director
+    public class Director:BaseEntity
     {
+
+        // bir sınıfın içinde başka sınıfın elemanlarını taşıyorsam ctorda çağırayım
+        public Director()
+        {
+            Movies = new List<Movie>();
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }//nullable olabilir.
+
+        public string FullName => FirstName + " " + LastName;
+
+        //navigation property
+
+        //1 yönetmen,n çokça filmi olabilir
+        public virtual List<Movie> Movies { get; set; }
     }
 }
