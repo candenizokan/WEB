d24817d7f2f659c52dca1ce2f9eff17b98648434
Okan Candeniz <candenizokan@gmail.com>
2023-07-09 14:10:36 +0300

    basemap virtual

--- CoreCrud/Models/Mappings/Abstract/BaseMap.cs
+++ CoreCrud/Models/Mappings/Abstract/BaseMap.cs
@@ -5,11 +5,15 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 namespace CoreCrud.Models.Mappings.Abstract
 {
     // kalıtım vereceğim için abstract yaptım. doğrudan doğruya kullanılmasın kalıtım versin
+    //IEntityTypeConfiguration bu interface Mic.Efcore'dan gelir. Biz ayrıyetten bir paket indirmedik zaten Mic.Efcore.Sql içine gömülü geldiği için direkt olarak bu paketi indirdik.
+
+    //Abstract işaretlediğimiz için BaseMap sınıfı içindeki CONFIGURE metodu VIRTUAL işaretlediğimizde bu metod aynen kullanılabilir yada kalıtım alan diğer sınıflar içeriğini istediği gibi kullanabilir.
     public abstract class BaseMap<T> : IEntityTypeConfiguration<T> where T : BaseEntity
     {
-        public void Configure(EntityTypeBuilder<T> builder)
+        //abstract olduğu için değiştirilmeden kesinkes kullanılıyor. ben bunu istersen bu şekilde kullan istersen içini değiştir şeklinde değiştirmem lazım. bunun için virtual yaptım isteyen kullansın isteyen değiştirip kullansın demiş oldum bu şekilde
+        public virtual void Configure(EntityTypeBuilder<T> builder)
         {
-            throw new System.NotImplementedException();
+            
         }
     }
 }
