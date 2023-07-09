f409bd049dc28132736e2356194d9d5404658ef4
Okan Candeniz <candenizokan@gmail.com>
2023-07-09 13:44:21 +0300

    actor ctor

--- CoreCrud/Models/Concrete/Actor.cs
+++ CoreCrud/Models/Concrete/Actor.cs
@@ -1,14 +1,24 @@
 ﻿using CoreCrud.Models.Abstract;
 using System;
+using System.Collections.Generic;
 
 namespace CoreCrud.Models.Concrete
 {
     public class Actor : BaseEntity
     {
+        public Actor()
+        {
+            MovieActors = new List<MovieActor>();
+        }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime? BirthDate { get; set; }//nullable olabilir.
 
         public string FullName => FirstName + " " + LastName;
+
+        //navigation property
+
+        //1 oyuncunun çokça filmi olabilir
+        public virtual List<MovieActor> MovieActors { get; set; }
     }
 }
