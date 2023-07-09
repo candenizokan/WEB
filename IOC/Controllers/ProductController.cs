4cfeec945f16afab2a4ee5091315e49aacc153d4
Okan Candeniz <candenizokan@gmail.com>
2023-07-09 10:31:50 +0300

    DI

--- IOC/Controllers/ProductController.cs
+++ IOC/Controllers/ProductController.cs
@@ -1,14 +1,17 @@
-﻿using IOC.Models.VMs;
+﻿using IOC.Infrastructure.Repositories.Abstract;
+using IOC.Models;
+using IOC.Models.VMs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IOC.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly IRepository<Category> _crepo;
 
-        public ProductController()
+        public ProductController(IRepository<Category> crepo)
         {
-
+            _crepo = crepo;
         }
 
        [HttpGet]//defaultta httpget olarak kabul eder
