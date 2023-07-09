9d1f31e69a10190cb315097f3ec3171d3d024d5d
Okan Candeniz <candenizokan@gmail.com>
2023-07-09 13:25:10 +0300

    add movie and director

---
+++ CoreCrud/Models/Concrete/Movie.cs
@@ -0,0 +1,18 @@
+﻿using CoreCrud.Models.Abstract;
+using System;
+
+namespace CoreCrud.Models.Concrete
+{
+    public class Movie:BaseEntity
+    {
+        public string Name { get; set; }
+        public DateTime PublishDate { get; set; }
+
+        //navigation property
+
+        //1 filmin 1 yönetmeni vardır
+
+        public int DirectorId { get; set; }
+        public Director Director { get; set; }
+    }
+}
