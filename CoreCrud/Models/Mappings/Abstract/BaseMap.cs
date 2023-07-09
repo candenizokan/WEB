e78c27990848d2f80ee38d657c886c772e21ab68
Okan Candeniz <candenizokan@gmail.com>
2023-07-09 14:15:38 +0300

    basemap configure

--- CoreCrud/Models/Mappings/Abstract/BaseMap.cs
+++ CoreCrud/Models/Mappings/Abstract/BaseMap.cs
@@ -13,7 +13,10 @@ namespace CoreCrud.Models.Mappings.Abstract
         //abstract olduğu için değiştirilmeden kesinkes kullanılıyor. ben bunu istersen bu şekilde kullan istersen içini değiştir şeklinde değiştirmem lazım. bunun için virtual yaptım isteyen kullansın isteyen değiştirip kullansın demiş oldum bu şekilde
         public virtual void Configure(EntityTypeBuilder<T> builder)
         {
-            
+            builder.HasKey(a=>a.ID);//ID sistemin belirttiği bir isim, söylemeseydikde aslında pk olduğunu bilirdi ama yinede söyledik.
+            builder.Property(a => a.IsActive).IsRequired();//defaultta IsRequired true kabul edilir. kolonun null geçiliğ geçilmeyeceğine karar veririrz. true=> gerekli alan boş bırakılmaz. false => gerekli değil, boş geçilebilir kolon
+            builder.Property(a => a.UpdateDate).IsRequired(false);//boş geçilebilir bir alan olmalı
+            builder.Property(a => a.DeleteDate).IsRequired(false);
         }
     }
 }
