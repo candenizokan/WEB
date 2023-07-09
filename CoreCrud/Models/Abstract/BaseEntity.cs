0eedfbfc2100dabb651753f810098cddc7395a11
Okan Candeniz <candenizokan@gmail.com>
2023-07-09 13:21:31 +0300

    baseentity

--- CoreCrud/Models/Abstract/BaseEntity.cs
+++ CoreCrud/Models/Abstract/BaseEntity.cs
@@ -1,6 +1,21 @@
-﻿namespace CoreCrud.Models.Abstract
+﻿using System;
+
+namespace CoreCrud.Models.Abstract
 {
     public abstract class BaseEntity
     {
+        public int ID { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
+
+        private bool _isActive=true;//defaultta hepsi true. silmek aslında burada activitesini false yapmak
+
+        public bool IsActive
+        {
+            get { return _isActive; }
+            set { _isActive= value; }
+        }
+
     }
 }
