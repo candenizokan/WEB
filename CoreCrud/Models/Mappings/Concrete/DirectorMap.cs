6c9dbe6813605aa19149b1da6a4c651e9dae956d
Okan Candeniz <candenizokan@gmail.com>
2023-07-16 10:16:33 +0300

    override Configure

--- CoreCrud/Models/Mappings/Concrete/DirectorMap.cs
+++ CoreCrud/Models/Mappings/Concrete/DirectorMap.cs
@@ -1,9 +1,14 @@
 ﻿using CoreCrud.Models.Concrete;
 using CoreCrud.Models.Mappings.Abstract;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace CoreCrud.Models.Mappings.Concrete
 {
     public class DirectorMap:BaseMap<Director>
     {
+        public override void Configure(EntityTypeBuilder<Director> builder)
+        {
+            base.Configure(builder);
+        }
     }
 }
