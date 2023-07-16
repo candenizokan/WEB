fda7f0c5e755f7496514356f7d8fd7f6fd588b26
Okan Candeniz <candenizokan@gmail.com>
2023-07-16 11:14:25 +0300

    startup lasy loading

--- CoreCrud/Startup.cs
+++ CoreCrud/Startup.cs
@@ -1,5 +1,7 @@
+using CoreCrud.Models.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,12 @@ namespace CoreCrud
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ProjectContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("Default"));
+                opt.UseLazyLoadingProxies(true);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
