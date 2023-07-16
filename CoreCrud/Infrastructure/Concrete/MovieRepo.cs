9c58d52591da3898b6f5d0ddf51c3223d6cb0711
Okan Candeniz <candenizokan@gmail.com>
2023-07-16 12:59:04 +0300

    movieRepo

---
+++ CoreCrud/Infrastructure/Concrete/MovieRepo.cs
@@ -0,0 +1,13 @@
+﻿using CoreCrud.Infrastructure.Abstarct;
+using CoreCrud.Models.Concrete;
+using CoreCrud.Models.Context;
+
+namespace CoreCrud.Infrastructure.Concrete
+{
+    public class MovieRepo : BaseRepo<Movie>
+    {
+        public MovieRepo(ProjectContext context) : base(context)
+        {
+        }
+    }
+}
