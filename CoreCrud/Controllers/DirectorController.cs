cf9e61421a1c6a68ee151b9a7417922fd42e2e4b
Okan Candeniz <candenizokan@gmail.com>
2023-07-16 13:40:50 +0300

    controller

--- CoreCrud/Controllers/DirectorController.cs
+++ CoreCrud/Controllers/DirectorController.cs
@@ -4,6 +4,13 @@ namespace CoreCrud.Controllers
 {
     public class DirectorController : Controller
     {
-       
+       //önce boş bir form verilecek doldurup post edecğiz
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
     }
 }
