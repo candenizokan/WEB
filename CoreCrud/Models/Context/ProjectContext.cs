43b20c7293631d58a8cdb164cf5c9e6e1f9f4aa2
Okan Candeniz <candenizokan@gmail.com>
2023-07-16 11:07:37 +0300

    tablolar ve konfigurasyonlar

--- CoreCrud/Models/Context/ProjectContext.cs
+++ CoreCrud/Models/Context/ProjectContext.cs
@@ -1,6 +1,35 @@
-﻿namespace CoreCrud.Models.Context
+﻿using CoreCrud.Models.Concrete;
+using CoreCrud.Models.Mappings.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreCrud.Models.Context
 {
-    public class ProjectContext
+    public class ProjectContext:DbContext
     {
+        public ProjectContext(DbContextOptions<ProjectContext> options):base(options)
+        {
+
+        }
+
+        //ayağa kalkacak tablolarımı yazacağım
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Director> Directors { get; set; }
+
+        public DbSet<Actor> Actors { get; set; }
+
+        public DbSet<MovieActor> MovieActors { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //konfigurasyonlarımıda söylüyorum
+            modelBuilder.ApplyConfiguration(new DirectorMap());
+            modelBuilder.ApplyConfiguration(new MovieActorMap());
+            modelBuilder.ApplyConfiguration(new MovieMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
