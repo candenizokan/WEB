4bcad16cfc46fc7875108b6ea897e94542e9eef6
Okan Candeniz <candenizokan@gmail.com>
2023-07-16 10:45:49 +0300

    MovieMap ilişkiler

---
+++ CoreCrud/Models/Mappings/Concrete/MovieMap.cs
@@ -0,0 +1,15 @@
+﻿using CoreCrud.Models.Concrete;
+using CoreCrud.Models.Mappings.Abstract;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoreCrud.Models.Mappings.Concrete
+{
+    public class MovieMap:BaseMap<Movie>
+    {
+        public override void Configure(EntityTypeBuilder<Movie> builder)
+        {
+            builder.HasOne(a => a.Director).WithMany(a=>a.Movies).HasForeignKey(a=>a.DirectorId);
+            base.Configure(builder);
+        }
+    }
+}
