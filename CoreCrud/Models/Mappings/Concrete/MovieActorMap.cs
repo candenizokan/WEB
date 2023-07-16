b0e47db5969d5eba887e542e619c3d560d29a40e
Okan Candeniz <candenizokan@gmail.com>
2023-07-16 10:28:13 +0300

    MovieActorMap

---
+++ CoreCrud/Models/Mappings/Concrete/MovieActorMap.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreCrud.Models.Mappings.Concrete
+{
+    public class MovieActorMap
+    {
+        //ara tablo bu. ayrı ayrı map yapmaktansa MovieActorMap üzerinden konuşturabilirim. MovieActorMap Base MapTen kalıtım almadığından IEntityTypeConfiguration den implemente edecek. bir sınıf IEntityTypeConfiguration dan kalıtım aldığında sql tarafında nasıl ayağa kaldıracağımı konfigure ediyorum. MovieActorMap bu ara tablo normaldede basemap'ten kalıtım almıyor. 
+    }
+}
