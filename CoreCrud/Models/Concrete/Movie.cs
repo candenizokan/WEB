ddff511f6f4213feceec4adc80bcec9ea9b1c9da
Okan Candeniz <candenizokan@gmail.com>
2023-07-16 10:40:58 +0300

    ilişkileri kuruyorum

--- CoreCrud/Models/Concrete/Movie.cs
+++ CoreCrud/Models/Concrete/Movie.cs
@@ -9,7 +9,7 @@ namespace CoreCrud.Models.Concrete
         //Bu sınıftan bir instance alındığında listenin varlığı olsun.ctorda yapıyorum
         public Movie()
         {
-            MovieActor = new List<MovieActor>();//movie instance alındığında kendi içinde list yapısı oluşmuş olarak gelir.
+            MovieActors = new List<MovieActor>();//movie instance alındığında kendi içinde list yapısı oluşmuş olarak gelir.
         }
 
         public string Name { get; set; }
@@ -23,6 +23,6 @@ namespace CoreCrud.Models.Concrete
         public virtual Director Director { get; set; }
 
         //1 filmin çokça oyuncusu vardır
-        public virtual List<MovieActor> MovieActor { get; set; }//virtual keywordü eklediğimde LAZY LOADING yağılmış olur. yeterli gemez. Mic.Efcore.Proxy paketi indirilmeli
+        public virtual List<MovieActor> MovieActors { get; set; }//virtual keywordü eklediğimde LAZY LOADING yağılmış olur. yeterli gemez. Mic.Efcore.Proxy paketi indirilmeli
     }
 }
--- CoreCrud/Models/Mappings/Concrete/MovieActorMap.cs
+++ CoreCrud/Models/Mappings/Concrete/MovieActorMap.cs
@@ -1,7 +1,18 @@
-﻿namespace CoreCrud.Models.Mappings.Concrete
+﻿using CoreCrud.Models.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoreCrud.Models.Mappings.Concrete
 {
-    public class MovieActorMap
+    public class MovieActorMap : IEntityTypeConfiguration<MovieActor>
     {
         //ara tablo bu. ayrı ayrı map yapmaktansa MovieActorMap üzerinden konuşturabilirim. MovieActorMap Base MapTen kalıtım almadığından IEntityTypeConfiguration den implemente edecek. bir sınıf IEntityTypeConfiguration dan kalıtım aldığında sql tarafında nasıl ayağa kaldıracağımı konfigure ediyorum. MovieActorMap bu ara tablo normaldede basemap'ten kalıtım almıyor. 
+        public void Configure(EntityTypeBuilder<MovieActor> builder)
+        {
+            builder.HasKey(a => new { a.ActorId, a.MovieId });//ikiside primary key
+            builder.HasOne(a => a.Movie).WithMany(a=>a.MovieActors).HasForeignKey(a=>a.MovieId);
+            builder.HasOne(a => a.Actor).WithMany(a=>a.MovieActors).HasForeignKey(a=>a.ActorId);
+            throw new System.NotImplementedException();
+        }
     }
 }
