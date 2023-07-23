4f0bb65e973f25557a8ffbfd55b5c0ed5b855c90
Okan Candeniz <candenizokan@gmail.com>
2023-07-23 12:07:56 +0300

    createmovievm note

--- CoreCrud/Views/VMs/CreateMovieVM.cs
+++ CoreCrud/Views/VMs/CreateMovieVM.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using CoreCrud.Models.DTOs;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 
@@ -7,12 +8,12 @@ namespace CoreCrud.Views.VMs
     public class CreateMovieVM
     {
         //MOVİE--film için iki input açarım kullanıcının yazdığını alırım
-        public string Name { get; set; }
-        public DateTime PublishDate { get; set; }
+        public string Name { get; set; } // kullanıcıdan almam gerek. bu durumda validasyon gerekiyor.
+        public DateTime PublishDate { get; set; } // kullanıcıdan almam gerek. bu durumda validasyon gerekiyor.
 
         //DIRECTOR-- bir filmin bir directorunu seçmeli. on tanesini götüreyim taşıyım o içinden bir tane seçsin. o seçileneni almam için DirectorID lazım
         public int DirectorID { get; set; }
-        public List<SelectListItem> Directors { get; set; }
+        public List<SelectListItem> Directors { get; set; } // bütün directorleri çağır selectlistitem nesnesi yarat
 
         //ACTOR--birden fazla actoru olabilir. o yüzden actorID tutmadım. aktörleri aktör olarak gönderme Actor sınıfını traşla yani dto yap. onu göster
 
