c05d1320bed469f4d609ff64edd13b580f6d366e
Okan Candeniz <candenizokan@gmail.com>
2023-07-23 12:14:05 +0300

    IDirector _dRepo;

--- CoreCrud/Controllers/MovieController.cs
+++ CoreCrud/Controllers/MovieController.cs
@@ -7,11 +7,13 @@ namespace CoreCrud.Controllers
     public class MovieController : Controller
     {
         private readonly IMovieRepo _mrepo;
+        private readonly IDirector _dRepo;
 
         //bir sınıf başkka bir sınıfa ihtiyaç duyuyorsa sınıfın ctorunda enjeksiyonla kullanmam lazım. ben senin soyut halin olan interface IMovieRepo tipinden birşey istiyorum. sen bana movie reponun sınıf halini vermen lazım. interface neyi nasıl yapacağını bilmez. bunun kodunu unutmadan startupta söylemem lazım. ben bunun soyut halini istediğimde bana somut halini ver demem lazım bunu startupda söylüyorum
-        public MovieController(IMovieRepo mrepo)
+        public MovieController(IMovieRepo mrepo, IDirector dRepo)
         {
             _mrepo = mrepo;
+            _dRepo = dRepo;
         }
 
 
