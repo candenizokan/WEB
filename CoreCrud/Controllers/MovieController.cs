151f2cb8069009057a654d387fd12052b32bfc80
Okan Candeniz <candenizokan@gmail.com>
2023-07-23 12:25:28 +0300

    actörleri çektim

--- CoreCrud/Controllers/MovieController.cs
+++ CoreCrud/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
 ﻿using CoreCrud.Infrastructure.Interfaces.Concrete;
+using CoreCrud.Models.DTOs;
 using CoreCrud.Views.VMs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -9,12 +10,14 @@ namespace CoreCrud.Controllers
     {
         private readonly IMovieRepo _mrepo;
         private readonly IDirector _dRepo;
+        private readonly IActorRepo _aRepo;
 
         //bir sınıf başkka bir sınıfa ihtiyaç duyuyorsa sınıfın ctorunda enjeksiyonla kullanmam lazım. ben senin soyut halin olan interface IMovieRepo tipinden birşey istiyorum. sen bana movie reponun sınıf halini vermen lazım. interface neyi nasıl yapacağını bilmez. bunun kodunu unutmadan startupta söylemem lazım. ben bunun soyut halini istediğimde bana somut halini ver demem lazım bunu startupda söylüyorum
-        public MovieController(IMovieRepo mrepo, IDirector dRepo)
+        public MovieController(IMovieRepo mrepo, IDirector dRepo, IActorRepo aRepo)
         {
             _mrepo = mrepo;
             _dRepo = dRepo;
+            _aRepo = aRepo;
         }
 
 
@@ -33,7 +36,13 @@ namespace CoreCrud.Controllers
                     expression:a => a.IsActive
                 ),// director repoya ihtiyacım oldu. o zaman ctorda director repoyu çağıracağım. GetByDefaults IBASEREPODA vardı. seçim parametrem var hangi property alıyorsun, koşulunu yaz parametren expression, sıralama yapabileceğin parametren var. güncelde aktif olan kullanıcıları where yazacağım.
 
-               
+               //sahip olduğum tüm aktörleri getirmem lazım. aktör repoya ihtiyacım oldu.
+
+                Actors = _aRepo.GetByDefaults
+                (
+                    selector:a=> new ActorDTO() { ActorID=a.ID, FullName=a.FullName,IsSelected=false },
+                    expression: a=> a.IsActive
+                )
             };
             return View();
         }
