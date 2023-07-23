9f17c6bbdc7d161aceb56b1cfe4fd2d5d4c8c806
Okan Candeniz <candenizokan@gmail.com>
2023-07-23 12:09:58 +0300

    cm dataannotations

--- CoreCrud/Views/VMs/CreateMovieVM.cs
+++ CoreCrud/Views/VMs/CreateMovieVM.cs
@@ -2,16 +2,22 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoreCrud.Views.VMs
 {
     public class CreateMovieVM
     {
         //MOVİE--film için iki input açarım kullanıcının yazdığını alırım
+
+        [Required(ErrorMessage ="İsimsiz film olamaz.")]
         public string Name { get; set; } // kullanıcıdan almam gerek. bu durumda validasyon gerekiyor.
+
+        [Required(ErrorMessage = "Çıkış tarihi boş olamaz.")]
         public DateTime PublishDate { get; set; } // kullanıcıdan almam gerek. bu durumda validasyon gerekiyor.
 
         //DIRECTOR-- bir filmin bir directorunu seçmeli. on tanesini götüreyim taşıyım o içinden bir tane seçsin. o seçileneni almam için DirectorID lazım
+        [Required(ErrorMessage = "Yönetmen Seçiniz")]
         public int DirectorID { get; set; }
         public List<SelectListItem> Directors { get; set; } // bütün directorleri çağır selectlistitem nesnesi yarat
 
