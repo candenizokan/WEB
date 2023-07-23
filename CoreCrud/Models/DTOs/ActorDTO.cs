f57c016e1609c8b5cdcde54fb57948d0f90746d7
Okan Candeniz <candenizokan@gmail.com>
2023-07-23 12:05:57 +0300

    actorDto

--- CoreCrud/Models/DTOs/ActorDTO.cs
+++ CoreCrud/Models/DTOs/ActorDTO.cs
@@ -2,6 +2,10 @@
 {
     public class ActorDTO
     {
+        //kullanıcıya has validasyon kontrolüne gerek yok. saten dolu olarak viewa gidecek bunlar yapıldı.
 
+        public string FullName { get; set; }//kullanıcıya göstemek için
+        public int ActorID { get; set; }//arka planda almak için ıd sini alıyorum
+        public bool IsSelected { get; set; }//seçilip seçilmediğini almak için
     }
 }
