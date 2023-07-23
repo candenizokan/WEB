1e04e91ba0833f9bdb39aa790366def85aa6f700
Okan Candeniz <candenizokan@gmail.com>
2023-07-23 11:37:13 +0300

    ctor _mrepo

---
+++ CoreCrud/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using CoreCrud.Infrastructure.Interfaces.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCrud.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly IMovieRepo _mrepo;
+
+        //bir sınıf başkka bir sınıfa ihtiyaç duyuyorsa sınıfın ctorunda enjeksiyonla kullanmam lazım. ben senin soyut halin olan interface IMovieRepo tipinden birşey istiyorum
+        public MovieController(IMovieRepo mrepo)
+        {
+            _mrepo = mrepo;
+        }
+
+
+
+
+    }
+}
