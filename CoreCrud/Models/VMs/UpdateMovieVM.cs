8f4bfe11be068a9c77a7eb4fa3d812f5e41f7d76
Okan Candeniz <candenizokan@gmail.com>
2023-07-29 10:38:22 +0300

    public int MovieID { get; set; }

--- CoreCrud/Models/VMs/UpdateMovieVM.cs
+++ CoreCrud/Models/VMs/UpdateMovieVM.cs
@@ -1,6 +1,19 @@
-﻿namespace CoreCrud.Models.VMs
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreCrud.Models.VMs
 {
     public class UpdateMovieVM
     {
+        //Movie
+
+        //sıfırdan film yaratmı9yorum. ilgili nesnenin id sine gerek var burada. kimi güncelleyeceğimi bilmem için id lazım onu getirmem lazım. 
+        public int MovieID { get; set; }//post'a düşüldüğünde hangi filmi güncelleyeceğini bilmeliyim
+
+        [Required(ErrorMessage = "İsimsiz film olamaz.")]
+        public string Name { get; set; } // kullanıcıdan almam gerek. bu durumda validasyon gerekiyor.
+
+        [Required(ErrorMessage = "Çıkış tarihi boş olamaz.")]
+        public DateTime PublishDate { get; set; } // kullanıcıdan almam gerek. bu durumda validasyon gerekiyor.
     }
 }
