1f9724b805377e75bb81401e2bae4a849615c2ce
Okan Candeniz <candenizokan@gmail.com>
2023-07-29 13:48:48 +0300

    Invoke

--- CoreCrud/Views/Shared/Components/MovieWithDirectorTop/MovieWithDirectorTopViewComponent.cs
+++ CoreCrud/Views/Shared/Components/MovieWithDirectorTop/MovieWithDirectorTopViewComponent.cs
@@ -1,6 +1,16 @@
-﻿namespace CoreCrud.Views.Shared.Components.MovieWithDirectorTop
+﻿using CoreCrud.Models.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreCrud.Views.Shared.Components.MovieWithDirectorTop
 {
-    public class MovieWithDirectorTopViewComponent
+    public class MovieWithDirectorTopViewComponent : ViewComponent
     {
+        //en çok filmi olan 3 yönetmeni alalım.
+
+        public IViewComponentResult Invoke()
+        {
+            List<Director> list =
+        }
     }
 }
