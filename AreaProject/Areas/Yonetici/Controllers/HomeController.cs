eba72c8b25b8f8a2229f7a7796f7014749fec472
Okan Candeniz <candenizokan@gmail.com>
2023-07-30 12:48:33 +0300

    areas yonetici and layout edit

---
+++ AreaProject/Areas/Yonetici/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AreaProject.Areas.Yonetici.Controllers
+{
+
+    //şu an hangi areada olduğunu yazman lazım. yoksa route'da görmez. sen burada garanti veeriyorsun. sen bir areasın diyorsun.
+
+    [Area("Yonetici")]
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
