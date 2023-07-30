fece936218ab5056a50115f38357bb1ce4b4f5e6
Okan Candeniz <candenizokan@gmail.com>
2023-07-30 12:37:31 +0300

    [Area("Admin")]

--- AreaProject/Areas/Admin/Controllers/HomeController.cs
+++ AreaProject/Areas/Admin/Controllers/HomeController.cs
@@ -2,8 +2,13 @@
 
 namespace AreaProject.Areas.Admin.Controllers
 {
+
+    //şu an hangi areada olduğunu yazman lazım. yoksa route'da görmez. sen burada garanti veeriyorsun. sen bir areasın diyorsun.
+
+    [Area("Admin")]
     public class HomeController : Controller
     {
+        
         public IActionResult Index()
         {
             return View();
